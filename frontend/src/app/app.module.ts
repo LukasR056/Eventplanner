e162ffbbb26a38e562e0067c2de4809a9684219e
LukasR <lukas.reitbauer@edu.fh-joanneum.at>
2020-01-21 20:04:49 +0100

    hoffentlich fertig merge

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,25 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {DateComponent} from './date/date.component';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { TaskListComponent } from './task-list/task-list.component';
-import { TaskFormComponent } from './task-form/task-form.component';
-import { ForumentryListComponent } from './forumentry-list/forumentry-list.component';
-import { ForumentryFormComponent } from './forumentry-form/forumentry-form.component';
-import { EventListComponent } from './event-list/event-list.component';
-import { EventFormComponent } from './event-form/event-form.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { UserFormComponent } from './user-form/user-form.component';
-import { TagListComponent } from './tag-list/tag-list.component';
-import { TagFormComponent } from './tag-form/tag-form.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {TaskListComponent} from './task-list/task-list.component';
+import {TaskFormComponent} from './task-form/task-form.component';
+import {ForumentryListComponent} from './forumentry-list/forumentry-list.component';
+import {ForumentryFormComponent} from './forumentry-form/forumentry-form.component';
+import {EventListComponent} from './event-list/event-list.component';
+import {EventFormComponent} from './event-form/event-form.component';
+import {UserListComponent} from './user-list/user-list.component';
+import {UserFormComponent} from './user-form/user-form.component';
+import {TagListComponent} from './tag-list/tag-list.component';
+import {TagFormComponent} from './tag-form/tag-form.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
-  MatAutocompleteModule,MatButtonToggle,MatCheckboxModule,
-  MatButtonModule, MatCardModule, MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDividerModule, MatExpansionModule,
+  MatAutocompleteModule, MatButtonToggle, MatCheckboxModule,
+  MatButtonModule, MatCardModule, MatChipsModule, MatDatepickerModule, MatDividerModule, MatExpansionModule,
   MatFormFieldModule, MatIconModule,
   MatInputModule, MatListModule,
   MatMenuModule, MatNativeDateModule,
@@ -27,30 +27,31 @@ import {
   MatTableModule, MatButtonToggleModule,
   MatToolbarModule,
 } from '@angular/material';
-import { FriendsFormComponent } from './friends-form/friends-form.component';
-import { FriendsListComponent } from './friends-list/friends-list.component';
-import { EventDetailComponent } from './event-detail/event-detail.component';
-import { HomepageComponent } from './homepage/homepage.component';
+import {FriendsFormComponent} from './friends-form/friends-form.component';
+import {FriendsListComponent} from './friends-list/friends-list.component';
+import {EventDetailComponent} from './event-detail/event-detail.component';
+import {HomepageComponent} from './homepage/homepage.component';
 
 import {DragDropModule} from '@angular/cdk/drag-drop';
-import { EventSearchComponent } from './event-search/event-search.component';
+import {EventSearchComponent} from './event-search/event-search.component';
 import {JwtModule} from '@auth0/angular-jwt';
-import { LoginComponent } from './login/login.component';
-import { LogoutComponent } from './logout/logout.component';
+import {LoginComponent} from './login/login.component';
+import {LogoutComponent} from './logout/logout.component';
 import {CalendarModule, DateAdapter} from 'angular-calendar';
 import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
-import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
-import { CalenderComponent } from './calender/calender.component';
-import { MediainputComponent } from './mediainput/mediainput.component';
+import {NgbModalModule} from '@ng-bootstrap/ng-bootstrap';
+import {CalenderComponent} from './calender/calender.component';
+import {MediainputComponent} from './mediainput/mediainput.component';
 import {FileUploadModule} from 'ng2-file-upload';
-import { RegistrationComponent } from './registration/registration.component';
+import {RegistrationComponent} from './registration/registration.component';
 import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
-import { CheckInvitationComponent } from './check-invitation/check-invitation.component';
+import {CheckInvitationComponent} from './check-invitation/check-invitation.component';
 
 
 export function tokenGetter() {
   return localStorage.getItem('access_token');
 }
+
 
 @NgModule({
   declarations: [
@@ -73,17 +74,16 @@ export function tokenGetter() {
     EventSearchComponent,
     LoginComponent,
     LogoutComponent,
-    CalenderComponent,
-    MediainputComponent,
     RegistrationComponent,
     CheckInvitationComponent,
+    CalenderComponent,
+    MediainputComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
-    FileUploadModule,
     BrowserAnimationsModule,
     MatTableModule,
     MatButtonModule,
@@ -98,25 +98,26 @@ export function tokenGetter() {
     MatCheckboxModule,
     MatIconModule,
     MatDividerModule,
-    NgbModalModule,
     MatExpansionModule,
-    MatButtonToggleModule,
     MatListModule,
     FormsModule,
     DragDropModule,
     MatAutocompleteModule,
     MatChipsModule,
+    MatButtonToggleModule,
+    NgxMaterialTimepickerModule,
+    CalendarModule.forRoot({provide: DateAdapter, useFactory: adapterFactory}),
     JwtModule.forRoot({
       config: {
         tokenGetter,
         whitelistedDomains: ['localhost:4200']
       }
     }),
+    FileUploadModule,
 
-    CalendarModule.forRoot({provide: DateAdapter, useFactory: adapterFactory}),
-    MatButtonToggleModule,
-    NgxMaterialTimepickerModule,
-  providers: [  ],
+  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
