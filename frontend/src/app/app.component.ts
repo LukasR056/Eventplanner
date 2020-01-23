dc90ad49edeb7dd6b30d799aa6ac3e6073966395
fetch <45804417+schwingermax@users.noreply.github.com>
2020-01-23 23:48:17 +0100

    Upload wird angezeigt

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {ActivatedRoute, Router} from '@angular/router';
+import {UserService} from './service/user.service';
 
 
 @Component({
@@ -6,12 +10,39 @@ import {Component} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   title = 'frontend';
+  pictures: number;
+  userFormGroup: any;
+  friendOptions: any;
+  userId: any;
+  user: any;
 
-  constructor() {
+  constructor(private fb: FormBuilder, private userService: UserService) {
   }
 
+  ngOnInit() {
+    this.userId = Number(localStorage.getItem('user_id'));
+    //this.pictures = this.userId.pictures;
+    this.userService.retrieveUserOptions().subscribe((result) => {
+      this.friendOptions = result;
+    });
+
+    this.userService.getUserById(this.userId)
+      .subscribe((response: any) => {
+        this.user = response;
+        this.user.id = response.id;
+        this.pictures = response.pictures;
+        console.log('sag mir Id Bruda: ' + this.user.id);
+        console.log('zeig schwanz Bruda: ' + this.pictures );
+      });
+
+
+
+
+  }
+
+
+
+
 }
-
-
