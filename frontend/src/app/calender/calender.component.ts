a2ae6fb1335188bdb2d49fdf68f87120d17a961a
cynthiazeya <cynthia.zeya@edu.fh-joanneum.at>
2020-01-24 13:37:28 +0100

    Revert "Resolver einsetzen probieren"
    This reverts commit 3c2a4923734ae583263644fa138a790bb6790580.

--- frontend/src/app/calender/calender.component.ts
+++ frontend/src/app/calender/calender.component.ts
@@ -4,7 +4,7 @@ import {Subject} from 'rxjs';
 import {CalendarEvent, CalendarView} from 'angular-calendar';
 import {HttpClient} from '@angular/common/http';
 import {EventService} from '../service/event.service';
-import {Router, ActivatedRoute} from '@angular/router';
+import {Router} from '@angular/router';
 
 const colors: any = {
   red: {
@@ -20,45 +20,71 @@ interface eventInterface {
 
 @Component({
   selector: 'app-calender',
+  // changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './calender.component.html',
   styleUrls: ['./calender.component.scss']
 })
 export class CalenderComponent implements OnInit, AfterContentChecked {
 
-  constructor(private http: HttpClient, private eventService: EventService, private router: Router, private route: ActivatedRoute) {}
+  constructor(private http: HttpClient, private eventService: EventService, private router: Router) {}
 
     @ViewChild('modalContent', { static: true }) modalContent: TemplateRef<any>;
 
   view: CalendarView = CalendarView.Month;
   eventName;
+  //event = this.eventService.getEventWithId(2).subscribe(value => {this.eventName = value.name;
+                                                                  // console.log('eventname: ' + this.eventName);
+                                                                  // console.log('value.name: ' + value.name)} );
+
   eventNames = [];
   eventDates = [];
   eventAll: eventInterface;
   eventAllList = [];
   all = [];
   allInterface: eventInterface;
-  eventNames2;
-  /*eventNames2 = this.eventService.getEvents().subscribe((response: any[]) => {
+
+ /* events = this.eventService.getEvents().subscribe(value => {
+    for (const e in value) {
+      console.log('e :' + e);
+    }
+    console.log('sollte gehen ' + value);
+    console.log('event names: ' + this.eventNames);
+  }); */
+ eventNames2 = this.eventService.getEvents().subscribe((response: any[]) => {
    (response.forEach(eventx => {this.eventNames.push(eventx.name);
+                                // this.eventYears.push(eventx.date.year);
+                                // console.log('jahr: ' + eventx.date[0] + eventx.date[1] + eventx.date[2] + eventx.date[3]);
+                                // this.eventYears.push(eventx.date[0] + eventx.date[1] + eventx.date[2] + eventx.date[3]);
                                 this.eventDates.push(eventx.date);
+                                // this.eventAll.name = eventx.name;
+                                // this.eventAll.date = eventx.date;
                                 this.eventAll = {name: eventx.name, date: eventx.date};
                                 this.eventAllList.push(this.eventAll);
 
      }));
+   /*console.log('drinnen: ' + this.eventNames);
+   console.log('drinnen: ' + this.eventDates);
+   console.log('ALLES INTERFACE: ' + this.eventAll);
+   console.log('ALLES ALLES: ' + this.eventAllList[0].name);*/
 
    for (let ev of this.eventAllList) {
+
+     //console.log('ALLE NAMEN ' + ev.name);
+     //console.log(ev);
 
      this.allInterface = {name: ev.name, date: ev.date};
      this.all.push(this.allInterface);
 
+     // console.log('ALL ALL ALL ' + this.all);
    }
-});*/
+});
+ 
   CalendarView = CalendarView;
   viewDate: Date = new Date();
   refresh: Subject<any> = new Subject();
   events1: Array<CalendarEvent> = [];
-  activeDayIsOpen = false;
 
+  activeDayIsOpen = false;
     ngOnInit() {
       /*this.eventService.getEvents()
         .subscribe((response: any[]) => {
@@ -66,23 +92,6 @@ export class CalenderComponent implements OnInit, AfterContentChecked {
         });
 
       this.eventName = this.event.name;*/
-
-      const data = this.route.snapshot.data;
-      this.eventNames = data.events;
-      this.eventNames2 = this.eventNames.forEach(eventx => {this.eventNames.push(eventx.name);
-        this.eventDates.push(eventx.date);
-        this.eventAll = {name: eventx.name, date: eventx.date};
-        this.eventAllList.push(this.eventAll);
-
-      });
-
-      for (let ev of this.eventAllList) {
-
-        this.allInterface = {name: ev.name, date: ev.date};
-        this.all.push(this.allInterface);
-
-      }
-
 
     }
 
@@ -92,6 +101,7 @@ export class CalenderComponent implements OnInit, AfterContentChecked {
 
   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
     if (isSameMonth(date, this.viewDate)) {
+      // console.log('eventName unten ' + this.eventName);
       this.activeDayIsOpen = !((isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
         events.length === 0);
       this.viewDate = date;
@@ -101,7 +111,7 @@ export class CalenderComponent implements OnInit, AfterContentChecked {
   setEvent() {
 
       if (this.events1.length === this.eventNames.length) {
-
+        console.log('gleich lang');
       } else {
 
     for (const en of this.eventAllList) {
@@ -115,7 +125,7 @@ export class CalenderComponent implements OnInit, AfterContentChecked {
         allDay: true,
 
       });
-
+      // console.log(en);
     }
       }
 
